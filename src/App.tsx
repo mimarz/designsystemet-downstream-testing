ddca7e2a06b9e2222f905cacf6d401463a650112
Michael Marszalek <mimarz@gmail.com>
2024-06-04 09:00:07 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "@digdir/designsystemet-theme";
+import "@digdir/designsystemet-css";
+import "./my-theme.css";
+
+import { Showcase } from "./showcase";
+
+function App() {
+  return <Showcase />;
+}
+
+export default App;
